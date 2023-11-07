17a8d8f7ea850d920fc1896a3e5dc7d75eb89bea
Simon Myrvig <simo008s@mail.hansenberg.dk>
2023-11-07 14:24:18 +0100

    Initial program

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    public void Run()
+    {
+        string dataDirectory = "Book_data";
+        List<Book> books = ReadBooksFromDirectory(dataDirectory);
+
+        Library myLibrary = new Library("My Library", books);
+        myLibrary.Start();
+    }
+
+    public List<Book> ReadBooksFromDirectory(string directoryPath)
+    {
+        List<Book> books = new List<Book>();
+
+        string[] bookFiles = Directory.GetFiles(directoryPath, "*.txt");
+        foreach (string bookFile in bookFiles)
+        {
+            string title = Path.GetFileNameWithoutExtension(bookFile);
+            string content = File.ReadAllText(bookFile);
+            books.Add(new Book(title, content));
+        }
+
+        return books;
+    }
+}
